17c4eb40140501548c87a9aa8f7d9f8d90883b1c
MemersonSt <emerson.cajape@tecsu.edu.ec>
2023-08-16 22:50:03 -0500

    Frontend

--- FrontendClient/src/main.jsx
+++ FrontendClient/src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import Sesion from "./Pages/Sesion.jsx";
+import Registrer from "./Pages/Registrer.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Sesion />
+
   </React.StrictMode>,
 )
